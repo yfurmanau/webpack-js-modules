e859a55e11d081c7bae4a62e8519e72389f587fa
yfurmanau <yfurmanau@gmail.com>
2024-06-07 18:33:48 +0200

    implement code splitting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { sum } from "./sum";
-import "./image-viewer";
-import "../styles/image-viewer.css";
+const button = document.createElement("button");
+button.innerText = "Click me";
+button.onclick = async () => {
+  const module = await import("./image-viewer");
+  module.default();
+};
 
-const total = sum(10, 5);
-
-console.log(total);
+document.body.appendChild(button);
